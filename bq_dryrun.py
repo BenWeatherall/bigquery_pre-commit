a8708bd366ec2d1321c8742146f4bba8a4c4fc2b
Ben Weatherall <Ben.Weatherall@petcircle.com.au>
2022-11-18 05:18:19 +1030

    replaced env variables with args

--- bq_dryrun.py
+++ bq_dryrun.py
@@ -1,15 +1,12 @@
 from __future__ import annotations
 
 import argparse
-import os
+import re
 import traceback
 from typing import Sequence
 
 from google.cloud import bigquery
 
-# Setup Template string replace
-TEMPLATE_FIND = r"__DATE_FILTER__"
-TEMPLATE_REPLACE = '= CURRENT_DATE("Australia/Sydney")'
 
 class bq:
     config = bigquery.QueryJobConfig(
@@ -23,19 +20,20 @@ class bq:
         )
 
     # Functions
-    def performDryRun(self, filename):
+    def performDryRun(self, filename, replacement_pairs):
         # OPEN FILE
         query_text = ""
         try:
-            with open(filename, 'r') as fi:
+            with open(filename, "r") as fi:
                 query_text = fi.read()
                 # perform replace
-                query_text = query_text.replace(TEMPLATE_FIND, TEMPLATE_REPLACE)
+                for pair in replacement_pairs:
+                    query_text = query_text.replace(pair[0], pair[1])
 
         except SyntaxError:
-            print(f'{filename} - Could not load query file')
+            print(f"{filename} - Could not load query file")
             print()
-            print('\t' + traceback.format_exc().replace('\n', '\n\t'))
+            print("\t" + traceback.format_exc().replace("\n", "\n\t"))
             print()
             return 1
 
@@ -45,33 +43,52 @@ class bq:
         )  # API request
 
         if query_job.errors or query_job.error_result:
-            print(f'{filename} - Dry run failed.')
+            print(f"{filename} - Dry run failed.")
             print()
-            print('\t' + traceback.format_exc().replace('\n', '\n\t'))
+            print("\t" + traceback.format_exc().replace("\n", "\n\t"))
             print()
             return 1
 
         # A dry run query completes immediately.
-        if 'total_bytes_processed' in query_job:
+        if "total_bytes_processed" in query_job:
             return 0
 
         return 1
 
+def findReplacePairs(args):
+    pairs = []
+    for arg in args:
+        matches = re.findall(r'^([^"=]+|"(?:\\.|[^"])*")=([^"=]+|"(?:\\.|[^"])*")$', arg)
+        if len(matches) == 2:
+            pairs.append((matches[0], matches[1]))
+        else:
+            print("Malformed Pairs")
+
+    return pairs
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("filenames", nargs="*", help="Filenames to run")
+    parser.add_argument("project", nargs="*", help="bq project")
+    parser.add_argument("region", nargs="*", help="bq region")
+    parser.add_argument("replace",
+                        metavar="FIND=REPLACE",
+                        nargs='+',
+                        help="Set a number of find and replace statements"
+                             "(if either value includes a space, define "
+                             "it with double quotes: "
+                             'replaceMe="replace Me". Note that '
+                             "values are always treated as strings.")
     args = parser.parse_args(argv)
 
     # Set Up Big Query
-    project = os.environ.get("PROJECT_DEV")
-    location = os.environ.get("REGION")
-    bigQuery = bq(project, location)
+    replacement_pairs = findReplacePairs(args.replace)
+    big_query = bq(args.project, args.region)
 
     # Perform Check
     retv = 0
     for filename in args.filenames:
-        retv |= bigQuery.performDryRun(filename)
+        retv |= big_query.performDryRun(filename, replacement_pairs)
     return retv
 
 
