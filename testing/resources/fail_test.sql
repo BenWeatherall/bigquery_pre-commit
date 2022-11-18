93593f639acb490a1591442280dee6730819aa8f
Ben Weatherall <Ben.Weatherall@petcircle.com.au>
2022-11-18 10:41:23 +1030

    starting to add testing

--- testing/resources/fail_test.sql
+++ testing/resources/fail_test.sql
@@ -1,4 +1,4 @@
 SELECT
     fake_field
-FROM `petcircle-datalake-cloud.c_m3ma.ordheader`
+FROM c_m3ma.ordheader
 LIMIT 1
--- testing/resources/pass_test.sql
+++ testing/resources/pass_test.sql
@@ -1,4 +1,4 @@
 SELECT
     orderid
-FROM `petcircle-datalake-cloud.c_m3ma.ordheader`
+FROM c_m3ma.ordheader
 LIMIT 1
